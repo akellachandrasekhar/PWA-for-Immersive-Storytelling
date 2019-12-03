4af5d48010bb6563db8f0f41cb2c760511a101eb
Chandra Akella <akellachandrasekhar@gmail.com>
2019-12-03 15:59:13 -0600

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -13,23 +13,6 @@ async function registerSW() {
   }
 }
 
-// Adds a custom popup which will indicate that our app can be added to home screen
-function displayPopUpOnIos() {
-	// Detects if device is on iOS 
-	const isIos = () => {
-	  const userAgent = window.navigator.userAgent.toLowerCase();
-	  return /iphone|ipad|ipod/.test( userAgent );
-	}
-	// Detects if device is in standalone mode
-	const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-	// Checks if should display install popup notification:
-	if (isIos() && !isInStandaloneMode()) {
-	  // this.setState({ showInstallMessage: true });
-	  showIosInstall();
-	}	
-}
-
 //code to prompt the user to allow notifications
 Notification.requestPermission(function(status) {
     if (Notification.permission == 'granted') {
@@ -88,3 +71,19 @@ function displayNotification() {
       iosPrompt.style.display = "none";
     });
   }
+// Adds a custom popup which will indicate that our app can be added to home screen
+function displayPopUpOnIos() {
+	// Detects if device is on iOS 
+	const isIos = () => {
+	  const userAgent = window.navigator.userAgent.toLowerCase();
+	  return /iphone|ipad|ipod/.test( userAgent );
+	}
+	// Detects if device is in standalone mode
+	const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+	// Checks if should display install popup notification:
+	if (isIos() && !isInStandaloneMode()) {
+	  // this.setState({ showInstallMessage: true });
+	  showIosInstall();
+	}	
+}
